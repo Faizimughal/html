7bed2529c8f6db361e128c6ec1f4f6dc35163814
Faizimughal <162406605+Faizimughal@users.noreply.github.com>
2024-05-01 00:40:40 +0500

    rotated box

---
+++ script.js
@@ -0,0 +1,51 @@
+// const calculations =(a , b)=>{
+// console.log("addition=" , a + b );
+// console.log("subtraction=" , a - b );
+// console.log("multiplication=" , a * b );
+// }
+// calculations(10,20);
+
+// const table=(a)=>{
+//     for(let i=1;i<=10;i++){
+//         console.log(a,"*",i,"=",a*i);
+//     }
+// }
+
+// table(8);
+
+// const quadratic=(a , b , c)=>{
+//     let d=b*b-4*a*c;
+//     if(d>0){
+//         let x1=(-b+Math.sqrt(d))/(2*a);
+//         let x2=(-b-Math.sqrt(d))/(2*a);
+//         console.log(x1,x2);
+//     }
+//     else if(d==0){
+//         let x=(-b)/(2*a);
+//         console.log(x);
+//     }
+//     else{
+//         console.log("no real roots");
+//     }
+// }
+
+// quadratic(10,20,3);
+
+
+// const areaofcircle=()=>{
+//     let r=10;
+//     let area=3.14*r*r;
+//     console.log(area);
+// }
+
+// areaofcircle();
+
+// const areaoftriangle=()=>{
+//     let b=10;
+//     let h=20;
+//     let area=0.5*b*h;
+//     console.log(area);
+// }
+
+// areaoftriangle();
+
